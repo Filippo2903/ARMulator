a478922bc622d4d16d021a9945fa0a5c7317dc16
alessandro-stella <alessandro.stella2004@gmail.com>
2025-05-13 11:48:18 +0200

    Reverting changes to avoid compiling errors

--- interface/static/js/comm.js
+++ interface/static/js/comm.js
@@ -32,179 +32,164 @@ ws.onmessage = function (event) {
         var obj = obj_list[idx];
 
         var element = document.getElementById(obj[0]);
+        if (obj[0] == "disassembly") {
+            $("#disassembly").html(obj[1]);
+        } else if (element != null) {
+            var target_value = obj[1];
 
-        switch (obj[0]) {
-            case "disassembly":
-                $("#disassembly").html(obj[1]);
-                break;
-
-            case "disable":
-                $("[name='" + obj[1] + "']").prop("disabled", true);
-                break;
-
-            case "edit_mode":
-                disableSim();
-                $("#assemble").text("Start").removeClass("assemble_edit");
-                refreshBreakpoints();
-                break;
-
-            case "line2addr":
-                line2addr = obj[1];
-                sendBreakpointsInstr();
-                break;
-
-            case "codeerror":
-                codeErrors.push({ row: obj[1], text: obj[2], type: "error" });
-                editor.session.setAnnotations(codeErrors);
-                break;
-
-            case "asm_breakpoints":
-                asm_breakpoints.length = 0;
-                editor.session.clearBreakpoints();
-                for (var i = 0; i < obj[1].length; i++) {
-                    editor.session.setBreakpoint(obj[1][i]);
-                    asm_breakpoints[i] = obj[1][i];
+            if ($.inArray("formatted_value", element.classList) >= 0) {
+                format_ = $("#valueformat").val();
+                if (format_ == "dec") {
+                    target_value = parseInt(obj[1], 16);
+                } else if (format_ == "decsign") {
+                    target_value = parseInt(obj[1], 16);
+                    if (target_value > Math.pow(2, 31) - 1) {
+                        target_value = target_value - Math.pow(2, 32);
+                    }
+                } else if (format_ == "bin") {
+                    target_value = parseInt(obj[1], 16).toString(2);
                 }
-                break;
+                if (isNaN(target_value)) {
+                    var target_value = obj[1];
+                }
+            }
 
-            case "nextline":
+            if (target_value == "False") {
+                target_value = "Faux";
+            }
+            if (target_value == "True") {
+                target_value = "Vrai";
+            }
+
+            $(element).val(target_value);
+            $(element).prop("disabled", false);
+        } else if (obj[0] == "disable") {
+            $("[name='" + obj[1] + "']").prop("disabled", true);
+        } else if (obj[0] == "edit_mode") {
+            disableSim();
+            $("#assemble").text("Démarrer").removeClass("assemble_edit");
+            refreshBreakpoints();
+        } else if (obj[0] == "line2addr") {
+            line2addr = obj[1];
+            sendBreakpointsInstr();
+        } else if (obj[0] == "codeerror") {
+            // row indices are 0-indexed
+            codeerrors.push({ row: obj[1], text: obj[2], type: "error" });
+            editor.session.setAnnotations(codeerrors);
+        } else if (obj[0] == "asm_breakpoints") {
+            asm_breakpoints.length = 0;
+            editor.session.clearBreakpoints();
+            for (var i = 0; i < obj[1].length; i++) {
+                editor.session.setBreakpoint(obj[1][i]);
+                asm_breakpoints[i] = obj[1][i];
+            }
+        } else if (obj[0] == "nextline") {
+            if (next_debug_marker !== null) {
+                editor.session.removeMarker(next_debug_marker);
+            }
+            next_debug_marker = editor.session.addMarker(
+                new aceRange(obj[1], 0, obj[1] + 1, 0),
+                "next_debug_line",
+                "text"
+            );
+            next_debugline = obj[1];
+        } else if (obj[0] == "debugline") {
+            if ($("#assemble").text() !== "Démarrer") {
+                $(".highlightread").removeClass("highlightread");
+                $(".highlightwrite").removeClass("highlightwrite");
+
+                mem_highlights_r.length = 0;
+                mem_highlights_w.length = 0;
+
+                if (debug_marker !== null) {
+                    editor.session.removeMarker(debug_marker);
+                }
                 if (next_debug_marker !== null) {
                     editor.session.removeMarker(next_debug_marker);
                 }
-                next_debug_marker = editor.session.addMarker(
-                    new aceRange(obj[1], 0, obj[1] + 1, 0),
-                    "next_debug_line",
-                    "text"
-                );
-                next_debugline = obj[1];
-                break;
-
-            case "debugline":
-                if ($("#assemble").text() !== "Start") {
-                    $(".highlightRead").removeClass("highlightRead");
-                    $(".highlightWrite").removeClass("highlightWrite");
-
-                    mem_highlights_r.length = 0;
-                    mem_highlights_w.length = 0;
-
-                    if (debug_marker !== null) {
-                        editor.session.removeMarker(debug_marker);
+                if (obj[1] >= 0) {
+                    aceRange = ace.require("ace/range").Range;
+                    debug_marker = editor.session.addMarker(
+                        new aceRange(obj[1], 0, obj[1] + 1, 0),
+                        "debug_line",
+                        "text"
+                    );
+                    if (
+                        current_debugline != obj[1] &&
+                        $("#follow_pc").is(":checked")
+                    ) {
+                        editor.scrollToLine(obj[1], true, true, function () {});
                     }
-                    if (next_debug_marker !== null) {
-                        editor.session.removeMarker(next_debug_marker);
-                    }
-                    if (obj[1] >= 0) {
-                        aceRange = ace.require("ace/range").Range;
-                        debug_marker = editor.session.addMarker(
-                            new aceRange(obj[1], 0, obj[1] + 1, 0),
-                            "debug_line",
-                            "text"
-                        );
-                        if (
-                            current_debugline != obj[1] &&
-                            $("#follow_pc").is(":checked")
-                        ) {
-                            editor.scrollToLine(
-                                obj[1],
-                                true,
-                                true,
-                                function () {}
-                            );
-                        }
-                        current_debugline = obj[1];
-                    } else {
-                        debug_marker = null;
-                    }
+                    current_debugline = obj[1];
+                } else {
+                    debug_marker = null;
                 }
-                break;
-
-            case "highlightread":
-            case "highlightwrite":
-                var type = obj[0].slice(9); // "read" o "write"
-                for (var i = 0; i < obj[1].length; i++) {
-                    var element = obj[1][i];
-                    try {
-                        if (!isNaN(element)) {
-                            // Highlight per memoria
-                            if (type == "read") {
-                                mem_highlights_r.push(element);
-                            } else {
-                                mem_highlights_w.push(element);
-                            }
+            }
+        } else if (obj[0].slice(0, 9) == "highlight") {
+            type = obj[0].slice(9);
+            for (var i = 0; i < obj[1].length; i++) {
+                var element = obj[1][i];
+                try {
+                    if (!isNaN(element)) {
+                        // Memory
+                        if (type == "read") {
+                            mem_highlights_r.push(element);
                         } else {
-                            // Highlight per registri
-                            $(document.getElementById(element)).addClass(
-                                "highlight" + type
-                            );
+                            mem_highlights_w.push(element);
                         }
-                    } catch (e) {}
-                }
-                break;
-
-            case "debuginstrmem":
-                if ($("#assemble").text() !== "Start") {
-                    mem_breakpoints_instr = obj[1];
-                    if ($("#follow_pc").is(":checked")) {
-                        var target = obj[1][0];
-                        var page = Math.floor(parseInt(target) / (16 * 20));
-                        if (editableGrid.getCurrentPageIndex() != page) {
-                            refresh_mem_paginator = true;
-                            editableGrid.setPageIndex(page);
-                        }
+                    } else {
+                        // Register
+                        $(document.getElementById(element)).addClass(
+                            "highlight" + type
+                        );
                     }
-                    editableGrid.refreshGrid();
-                }
-                break;
-
-            case "mempartial":
-                for (var i = 0; i < obj[1].length; i++) {
-                    var row = Math.floor(obj[1][i][0] / 16);
-                    var col = (obj[1][i][0] % 16) + 1;
-                    editableGrid.setValueAt(row, col, obj[1][i][1], false);
+                } catch (e) {}
+            }
+        } else if (obj[0] == "debuginstrmem") {
+            if ($("#assemble").text() !== "Démarrer") {
+                mem_breakpoints_instr = obj[1];
+                if ($("#follow_pc").is(":checked")) {
+                    var target = obj[1][0];
+                    var page = Math.floor(parseInt(target) / (16 * 20));
+                    if (editableGrid.getCurrentPageIndex() != page) {
+                        refresh_mem_paginator = true;
+                        editableGrid.setPageIndex(page);
+                    }
                 }
                 editableGrid.refreshGrid();
-                break;
-
-            case "mem":
-                refresh_mem_paginator = true;
-                editableGrid.load({ data: obj[1] });
-                break;
-
-            case "membp_r":
-                mem_breakpoints_r = obj[1];
-                break;
-
-            case "membp_w":
-                mem_breakpoints_w = obj[1];
-                break;
-
-            case "membp_rw":
-                mem_breakpoints_rw = obj[1];
-                break;
-
-            case "membp_e":
-                if ($("#assemble").text() !== "Start") {
-                    mem_breakpoints_e = obj[1];
-                }
-                break;
-
-            case "banking":
-                $("#tab-container").easytabs("select", "#tabs1-" + obj[1]);
-                if (obj[1] == "User") {
-                    $("#spsr_title").text("SPSR");
-                } else {
-                    $("#spsr_title").html("SPSR<br/>(" + obj[1] + ")");
-                }
-                break;
-
-            case "error":
-                displayErrorMsg(obj[1]);
-                break;
-
-            default:
-                console.log("Unknown command:");
-                console.log(obj);
-                break;
+            }
+        } else if (obj[0] == "mempartial") {
+            for (var i = 0; i < obj[1].length; i++) {
+                var row = Math.floor(obj[1][i][0] / 16);
+                var col = (obj[1][i][0] % 16) + 1;
+                editableGrid.setValueAt(row, col, obj[1][i][1], false);
+            }
+            editableGrid.refreshGrid();
+        } else if (obj[0] == "mem") {
+            refresh_mem_paginator = true;
+            editableGrid.load({ data: obj[1] });
+        } else if (obj[0] == "membp_r") {
+            mem_breakpoints_r = obj[1];
+        } else if (obj[0] == "membp_w") {
+            mem_breakpoints_w = obj[1];
+        } else if (obj[0] == "membp_rw") {
+            mem_breakpoints_rw = obj[1];
+        } else if (obj[0] == "membp_e") {
+            if ($("#assemble").text() !== "Démarrer") {
+                mem_breakpoints_e = obj[1];
+            }
+        } else if (obj[0] == "banking") {
+            $("#tab-container").easytabs("select", "#tabs1-" + obj[1]);
+            if (obj[1] == "User") {
+                $("#spsr_title").text("SPSR");
+            } else {
+                $("#spsr_title").html("SPSR<br/>(" + obj[1] + ")");
+            }
+        } else if (obj[0] == "error") {
+            displayErrorMsg(obj[1]);
+        } else {
+            console.log("Unknown command:");
+            console.log(obj);
         }
     }
 };
