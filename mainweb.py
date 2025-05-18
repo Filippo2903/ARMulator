9be65af3d47361e6a4389c0113507367f268948c
Alessandro Stella <alessandro.stella2004@gmail.com>
2025-05-18 03:54:49 +0200

    Changing simulatorFr to simulatorEn

--- mainweb.py
+++ mainweb.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ddd4fdda62dc3cca5756795b405f4aea39a992e619869f67283eb11b3880629
+oid sha256:8e57c94fc6c476012a60db53197b1b96de62991eb80fdf20e9f051cdb84435ca
 size 24504
